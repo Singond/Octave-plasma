e162729b7df44b84d536751b3f8e312e113576be
Jan Singon Slany <singond@seznam.cz>
2022-10-23 21:15:33 +0200

    Add tests for read_princeton_spe

--- test/test_read_princeton_spe.m
+++ test/test_read_princeton_spe.m
@@ -1,2 +1,19 @@
 %!error <Bad SPE header in>
 %! d = read_princeton_spe("data/starlab/energy.txt");
+
+%!test
+%! [~, d] = read_princeton_spe("data/princeton_spe/beam-headeronly.spe");
+%! assert(d.version, 2.2, 1e-6);
+%! assert(d.xdim, 1024);
+%! assert(d.ydim, 256);
+%! assert(d.numframes, 61);
+%! assert(d.datestr, "15Aug2022");
+%! assert(d.accum, 500);
+%! assert(d.readouttime, 0.26954, 0.0001);
+
+%!test
+%! [~, d] = read_princeton_spe("data/princeton_spe/discharge-headeronly.spe");
+%! assert(d.version, 3);
+%! assert(d.xdim, 1024);
+%! assert(d.ydim, 1024);
+%! assert(d.numframes, 50);
