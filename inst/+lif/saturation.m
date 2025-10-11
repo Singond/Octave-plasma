8d185c306dcba7474ce831fa7673d01fb8841426
Jan Singon Slany <singond@seznam.cz>
2025-10-11 22:55:48 +0200

    Document lif.saturation functions

--- inst/+lif/saturation.m
+++ inst/+lif/saturation.m
@@ -1,6 +1,33 @@
 ## -*- texinfo -*-
+## @deftypefn  {} {@var{R} =} lif.saturation (@var{S})
+## @deftypefnx {} {@var{R} =} lif.saturation (@var{S}, @var{opts})
 ##
-## @seealso{lif.saturationx}
+## Calculate the parameters of saturated fluorescence in each pixel
+## of a series of images @code{@var{S}.imgn} taken at laser energies
+## @code{@var{S}.E}.
+##
+## Return a copy of struct @var{S} with the following fields added:
+##
+## @table @code
+## @item  a
+## @itemx b
+## The fluorescence parameters @var{a} and @var{b} at each pixel.
+## Each of these is an array with the size equal to the first two
+## dimensions of @code{@var{S}.imgn}.
+##
+## @item fits
+## Information about the fits in the same form as in
+## @code{fit_fluorescence_int}.
+##
+## @item imgsm
+## Image @code{@var{S}.imgn} smoothed by the @qcode{"smooth"} parameter.
+## @end table
+##
+## Additional input arguments @var{opts} are passed directly into
+## @code{lif.fit_fluorescence_int}.
+##
+## @seealso{lif.fit_fluorescence_int, lif.saturationx}
+## @end deftypefn
 function result = saturation(S, varargin)
 	if (!isstruct(S))
 		print_usage;
--- inst/+lif/saturationx.m
+++ inst/+lif/saturationx.m
@@ -1,6 +1,40 @@
 ## -*- texinfo -*-
+## @deftypefn  {} {@var{R} =} lif.saturationx (@var{S})
+## @deftypefnx {} {@var{R} =} lif.saturationx (@var{S}, @var{opts})
 ##
-## @seealso{lif.saturation}
+## Calculate the parameters of saturated fluorescence in each column
+## of a series of images @code{@var{S}.imgn} taken at laser energies
+## @code{@var{S}.E}.
+##
+## Sum @code{@var{S}.imgn} in vertical direction and work on this sum.
+## This is like @code{lif.saturation}, but instead of fitting each pixel
+## separately, only the sum in each column is fitted.
+## This leads to faster computation and possibly more reliable fits due to
+## better signal-to-noise ratio, at the cost of losing the resolution
+## in y-direction.
+##
+## Return a copy of struct @var{S} with the following fields added:
+##
+## @table @code
+## @item  ax
+## @itemx bx
+## The fluorescence parameters @var{a} and @var{b} at each pixel.
+## Each of these is an array with the size equal to the first two
+## dimensions of @code{@var{S}.imgn}.
+##
+## @item fitsx
+## Information about the fits in the same form as in
+## @code{fit_fluorescence_int}.
+##
+## @item imgsmx
+## Image @code{@var{S}.imgn} smoothed by the @qcode{"smooth"} parameter.
+## @end table
+##
+## Additional input arguments @var{opts} are passed directly into
+## @code{lif.fit_fluorescence_int}.
+##
+## @seealso{lif.fit_fluorescence_int, lif.saturation}
+## @end deftypefn
 function result = saturationx(S, varargin)
 	if (!isstruct(S))
 		print_usage;
